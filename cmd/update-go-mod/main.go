1f5db56cb84095891396420430788d373e950602
Hau Nguyen <hauvipapro@gmail.com>
2023-08-15 16:47:52 +0700

    feat: add github client

--- cmd/update-go-mod/main.go
+++ cmd/update-go-mod/main.go
@@ -1,8 +1,49 @@
 package main
 
-import "github.com/haunt98/update-go-mod/internal/cli"
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/google/go-github/v53/github"
+	"golang.org/x/oauth2"
+
+	"github.com/make-go-great/netrc-go"
+
+	"github.com/haunt98/update-go-mod/internal/cli"
+)
+
+const (
+	netrcPath          = "~/.netrc"
+	netrcMachineGitHub = "github.com"
+)
 
 func main() {
-	app := cli.NewApp()
+	// Prepare GitHub
+	netrcData, err := netrc.ParseFile(netrcPath)
+	if err != nil {
+		log.Fatalln("Failed to read file netrc", err)
+	}
+
+	var ghAccessToken string
+	for _, machine := range netrcData.Machines {
+		if machine.Name == netrcMachineGitHub {
+			ghAccessToken = machine.Password
+			break
+		}
+	}
+	if ghAccessToken == "" {
+		log.Fatalln("Empty GitHub token in netrc")
+	}
+
+	ghTokenSrc := oauth2.StaticTokenSource(
+		&oauth2.Token{
+			AccessToken: strings.TrimSpace(ghAccessToken),
+		},
+	)
+	ghHTTPClient := oauth2.NewClient(context.Background(), ghTokenSrc)
+	ghClient := github.NewClient(ghHTTPClient)
+
+	app := cli.NewApp(ghClient)
 	app.Run()
 }
