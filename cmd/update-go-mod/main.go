4b4ec504dbd72d965ad9655ce44bba4f3858c201
Hau Nguyen <hauvipapro@gmail.com>
2026-07-07 09:44:52 +0700

    build: bump go deps
    - github.com/google/go-github/v89 v89.0.0

--- cmd/update-go-mod/main.go
+++ cmd/update-go-mod/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"strings"
 
-	"github.com/google/go-github/v88/github"
+	"github.com/google/go-github/v89/github"
 
 	"github.com/make-go-great/netrc-go"
 
--- internal/cli/action.go
+++ internal/cli/action.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"log"
 
-	"github.com/google/go-github/v88/github"
+	"github.com/google/go-github/v89/github"
 	"github.com/urfave/cli/v3"
 )
 
